934e5532f5a6471fbd3eb10749fab4599d32b0b5
StasScorobogatskii <114143870+Lordcreeper777@users.noreply.github.com>
2026-08-06 10:48:46 +0200

    Add animation to Mr. Bones NPC
    Idle Animation for NPC Mr.bones added + Refined Player animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,12 +13,28 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Animator anim;
+
     private bool isGrounded;
+    private bool animatorReady;
+    private float moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        animatorReady = false;
+
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isGrounded", true);
+        anim.SetFloat("yVelocity", 0f);
+
+        Invoke(nameof(EnableAnimatorUpdates), 0.1f);
+    }
+
+    void EnableAnimatorUpdates()
+    {
+        animatorReady = true;
     }
 
     void Update()
@@ -29,22 +45,49 @@ public class PlayerMovement : MonoBehaviour
             groundLayer
         );
 
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
-        // Flip character direction
-        if (moveInput > 0)
-            transform.localScale = new Vector3(0.4f, 0.4f, 1f);
-        else if (moveInput < 0)
-            transform.localScale = new Vector3(-0.4f, 0.4f, 1f);
-
-        // Switch animation
-        anim.SetBool("isWalking", moveInput != 0);
-        Debug.Log("isWalking: " + (moveInput != 0));
+        moveInput = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(
+                rb.velocity.x,
+                jumpForce
+            );
         }
+
+        if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(0.4f, 0.4f, 1f);
+        }
+        else if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(-0.4f, 0.4f, 1f);
+        }
+
+        if (animatorReady)
+        {
+            anim.SetBool("isWalking", moveInput != 0);
+            anim.SetBool("isGrounded", isGrounded);
+            anim.SetFloat("yVelocity", rb.velocity.y);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(
+            moveInput * moveSpeed,
+            rb.velocity.y
+        );
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null)
+            return;
+
+        Gizmos.DrawWireSphere(
+            groundCheck.position,
+            groundCheckRadius
+        );
     }
 }
