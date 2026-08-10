3b993ab75db08108c25489e6d1470e97b574ffda
StasScorobogatskii <114143870+Lordcreeper777@users.noreply.github.com>
2026-08-10 12:21:27 +0200

    feat: add persistent menu music and intro transitions

--- Assets/Scripts/QuoteFadeIn.cs
+++ Assets/Scripts/QuoteFadeIn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(CanvasGroup))]
 public class QuoteFadeIn : MonoBehaviour
@@ -7,6 +8,7 @@ public class QuoteFadeIn : MonoBehaviour
     public float fadeDuration = 1.5f;
 
     private CanvasGroup canvasGroup;
+    private bool isTransitioning = false;
 
     private void Awake()
     {
@@ -21,13 +23,38 @@ public class QuoteFadeIn : MonoBehaviour
         while (time < fadeDuration)
         {
             time += Time.unscaledDeltaTime;
-
-            canvasGroup.alpha =
-                1f - Mathf.Clamp01(time / fadeDuration);
+            canvasGroup.alpha = 1f - Mathf.Clamp01(time / fadeDuration);
 
             yield return null;
         }
 
         canvasGroup.alpha = 0f;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        if (!isTransitioning)
+        {
+            StartCoroutine(FadeOutAndLoad(sceneName));
+        }
+    }
+
+    private IEnumerator FadeOutAndLoad(string sceneName)
+    {
+        isTransitioning = true;
+
+        float time = 0f;
+
+        while (time < fadeDuration)
+        {
+            time += Time.unscaledDeltaTime;
+            canvasGroup.alpha = Mathf.Clamp01(time / fadeDuration);
+
+            yield return null;
+        }
+
+        canvasGroup.alpha = 1f;
+
+        SceneManager.LoadScene(sceneName);
     }
 }
